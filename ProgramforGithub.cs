844f7cdcbd86bd0b975f39ca69e6633317919b6e
Muhammad1510 <12300037@students.koi.edu.au>
2024-12-03 14:22:01 +1100

    Create ProgramforGithub.cs
    making 2 changes

---
+++ ProgramforGithub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SampleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Sum Calculator!");
+
+            // Input first number
+            Console.Write("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Input second number
+            Console.Write("Enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Calculate sum
+            int sum = num1 + num2;
+
+            // Display result
+            Console.WriteLine($"The sum of {num1} and {num2} is {sum}.");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
